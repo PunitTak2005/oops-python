e508a6e5dfbd65bc8735c787713d345f757f11bc
Punit Tak <147526916+PunitTak2005@users.noreply.github.com>
2024-11-09 15:02:11 +0530

    Create 21.py

---
+++ 21.py
@@ -0,0 +1,28 @@
+class Car:
+
+    def __init__(self,userbrand,usermodel):
+        self.brand = userbrand
+        self.model = usermodel
+    
+    def get_brand(self):
+        return self.brand + "!"
+    def full_name(self): 
+        return f"{self.brand}{self.model}"  
+
+class ElectricCar(Car):
+    def __init__(self,userbrand, usermodel, userbattery_size):
+        super().__init__(userbrand, usermodel)
+        self.battery_size = userbattery_size
+
+my_tesla = ElectricCar("Tesla","Model S", 120)
+print(my_tesla.full_name)
+print(my_tesla.get_brand())
+
+
+# my_car = Car("Toyota","Corolla")
+# print(my_car.brand)
+# print(my_car.model)
+# print(my_car.full_name)
+
+# my_new_car = Car("Tata","Safari")
+# print(my_new_car.model)
