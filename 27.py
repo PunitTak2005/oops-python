d384323fc08643d790ef10e58b392c75787de137
Punit Tak <147526916+PunitTak2005@users.noreply.github.com>
2024-11-09 15:37:24 +0530

    Create 27.py

---
+++ 27.py
@@ -0,0 +1,52 @@
+class Car:
+    total_car = 0
+    def __init__(self, userbrand, usermodel):
+        self.__brand = userbrand
+        self.model = usermodel
+        Car.total_car += 1
+
+    def get_brand(self):
+        return self.__brand + "!"
+    
+    def full_name(self): 
+        return f"{self.__brand} {self.model}"
+    
+    def fuel_type(self):
+        return "Petrol or Diesel"
+
+
+    def general_description(self):
+        return 'Cars are means of transportation'
+
+    @property
+    def model_name(self):
+        return self.__model
+
+
+class ElectricCar(Car):
+    def __init__(self, userbrand, usermodel, userbattery_size):
+        super().__init__(userbrand, usermodel)
+        self.battery_size = userbattery_size
+
+    def fuel_type(self):
+        return "Electric charge"
+
+my_tesla = ElectricCar("Tesla", "Model S", 120)
+print(my_tesla.fuel_type())
+print(my_tesla.general_description())
+
+Car("Tata", "safari")
+Car.model="city"
+Car("Tata", "Nexon")
+
+print(Car.total_car)
+
+my_car = Car("Toyota", "Corolla")
+print(my_car.get_brand())
+print(my_car.model)
+print(my_car.full_name())
+
+my_new_car = Car("Tata", "Safari")
+my_new_car.model="city"
+
+print(my_new_car.model)
